84bd9a43a24290721caf31ac6b74ded93976873e
sai1004 <saikiran.bavandla@gmail.com>
2020-06-07 13:08:34 +0530

    commit

--- video-upload/home/home.module.ts
+++ video-upload/home/home.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { IonicModule } from "ionic-angular";
+
+import { HomeComponent } from "./home.component";
+
+import { Camera } from "@ionic-native/camera";
+import { File } from "@ionic-native/file";
+import { FileTransfer } from "@ionic-native/file-transfer";
 
 @NgModule({
-    declarations: [],
-    imports: [ CommonModule ],
-    exports: [],
-    providers: [],
+  declarations: [HomeComponent],
+  imports: [IonicModule, CommonModule],
+  providers: [Camera, File, FileTransfer],
 })
+
 export class HomeModule {}
